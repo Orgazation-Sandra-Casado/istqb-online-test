a17ca1d5c150fc7233cd5823afebb4c863a606ec
lexadev0 <lexadev0@gmail.com>
2024-05-18 18:09:35 +0200

    https://lexaqa.atlassian.net/browse/IOT-1 wellcome

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-document.write("Hola Mundo");
+function imprimir(mensaje, id) {
+    let parrafo = document.getElementById(id);
+    parrafo.innerText = mensaje;
+}
+
+function wellcome() {
+    let name = document.getElementById('nameStudent').value;
+    let mensaje = 'Hi ' + name + ', wellcome to the test!';
+    imprimir(mensaje, 'wellcomeStudent');
+}
