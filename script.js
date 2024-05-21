66dad7138fb587e7ab00c51c3874738e13a20fc1
lexadev0 <lexadev0@gmail.com>
2024-05-21 15:01:59 +0200

    added function See Answer

--- script.js
+++ script.js
@@ -6,5 +6,13 @@ function imprimir(mensaje, id) {
 function wellcome() {
     let name = document.getElementById('nameStudent').value;
     let mensaje = 'Hi ' + name + ', wellcome to the test!';
+
     imprimir(mensaje, 'wellcomeStudent');
+}
+
+function seeAnswer(){
+    let ques1 = document.getElementById('question1correct').value;
+    let mensaje = 'La respuesta correcta es ' + ques1;
+
+    imprimir(mensaje, 'question1');
 }
