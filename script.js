5cfbcac1fb89db35868f2b90fa9d52ff6f42942a
lexadev0 <lexadev0@gmail.com>
2024-06-01 19:03:55 +0200

    IOT-3 correctAnswer

--- script.js
+++ script.js
@@ -11,13 +11,41 @@ function welcome() {
         mensaje = 'Hi ' + name + ', welcome to the test!';
     }
     else {
-        mensaje ='The name cannot be empty';
+        mensaje = 'The name cannot be empty';
     }
-    imprimir(mensaje, 'wellcomeStudent');
+    imprimir(mensaje, 'welcomeStudent');
 }
 
-function seeAnswer(){
+function seeAnswer() {
     let mensaje = 'The correct answer is C';
 
     imprimir(mensaje, 'seeAnswer');
+}
+
+function correctAnswer() {
+    let corregirRespuesta = document.getElementById('question1').value;
+    let mensaje = '';
+    if (corregirRespuesta === 'question1correct') {
+        mensaje = 'Correct';
+    } else if (corregirRespuesta === 'question1empty') {
+        mensaje = 'Cannot be empty';
+    }
+    else {
+        mensaje = 'Wrong!';
+    }
+    imprimir(mensaje, 'question1correct');
+}
+
+function seeScore() {
+    let puntuacion = document.getElementById('question1');
+    let total;
+    let mensaje = '';
+    if (puntuacion === 'question1correct') {
+        mensaje = total + 2;
+    } else if (puntuacion === 'question1empty') {
+        mensaje = total;
+    } else {
+        mensaje = total - 1;
+    }
+    imprimir(mensaje);
 }
